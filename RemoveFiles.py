74d7136898a03bd3ef1088cc57be2242b9facf1e
dan varughese <51270390+dan-varughese@users.noreply.github.com>
2021-05-01 16:06:01 -0400

    Add files via upload

---
+++ RemoveFiles.py
@@ -0,0 +1,13 @@
+import time
+import os 
+import shutil
+
+var1 = os.system('date')
+var2 = 'c:/Users/dan varughese/desktop'
+time.time(var1)
+os.path.exists(var2)
+os.walk(var2)
+os.path.join()
+ctime = os.stat(path).st_ctime
+
+return ctime
